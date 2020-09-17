a13091710957d54a39239fd402f6f99cb4f6168e
834H4R <71222128+834H4R@users.noreply.github.com>
2020-09-17 15:27:59 +0300

    Add files via upload

---
+++ Lab1/Main.java
@@ -0,0 +1,13 @@
+package com.Lab1;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Tasks lab1 = new Tasks();
+        lab1.Task1();
+        lab1.Task2();
+        lab1.Task3();
+        lab1.Task4();
+        lab1.Pract();
+    }
+}
