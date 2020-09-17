93f1e702deb1f69e613c4b3a9845377a7ef7cab7
834H4R <71222128+834H4R@users.noreply.github.com>
2020-09-17 15:18:23 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Tasks lab1 = new Tasks();
+        // lab1.Process1();
+        //lab1.Process2();
+        //lab1.Process3();
+        //lab1.Process4();
+        lab1.Pract();
+    }
+}
